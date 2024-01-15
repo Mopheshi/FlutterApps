d96c6129e87256f2491369e7a7377c9cd5b4ce31
mopheshi <ndachimya@gmail.com>
2024-01-15 09:18:51 +0100

    Implementing Signing in and OTP Verification with phone number...

--- lib/features/authentication/views/otp_verification_screen.dart
+++ lib/features/authentication/views/otp_verification_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pinput/pinput.dart';
 import 'package:todo_app/core/common/widgets/white_space.dart';
 import 'package:todo_app/core/res/colours.dart';
 import 'package:todo_app/core/res/media_res.dart';
@@ -13,21 +15,90 @@ class OTPVerificationScreen extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 30),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Image.asset(ImageResource.todo),
               const WhiteSpce(height: 26),
-              const Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colours.light,
-                      ),
-                    ),
+              // Basic manual implementation of OTP TextFields with a Row...
+              // const Row(
+              //   children: [
+              //     Expanded(
+              //       child: TextField(
+              //         decoration: InputDecoration(
+              //           filled: true,
+              //           fillColor: Colours.light,
+              //         ),
+              //       ),
+              //     ),
+              //     WhiteSpce(width: 5),
+              //     Expanded(
+              //       child: TextField(
+              //         decoration: InputDecoration(
+              //           filled: true,
+              //           fillColor: Colours.light,
+              //         ),
+              //       ),
+              //     ),
+              //     WhiteSpce(width: 5),
+              //     Expanded(
+              //       child: TextField(
+              //         decoration: InputDecoration(
+              //           filled: true,
+              //           fillColor: Colours.light,
+              //         ),
+              //       ),
+              //     ),
+              //     WhiteSpce(width: 5),
+              //     Expanded(
+              //       child: TextField(
+              //         decoration: InputDecoration(
+              //           filled: true,
+              //           fillColor: Colours.light,
+              //         ),
+              //       ),
+              //     ),
+              //     WhiteSpce(width: 5),
+              //     Expanded(
+              //       child: TextField(
+              //         decoration: InputDecoration(
+              //           filled: true,
+              //           fillColor: Colours.light,
+              //         ),
+              //       ),
+              //     ),
+              //     WhiteSpce(width: 5),
+              //     Expanded(
+              //       child: TextField(
+              //         decoration: InputDecoration(
+              //           filled: true,
+              //           fillColor: Colours.light,
+              //         ),
+              //       ),
+              //     ),
+              //     WhiteSpce(width: 5),
+              //   ],
+              // ),
+              // Auto implementation of OTP TextFields with PinPut library...
+              Pinput(
+                length: 6,
+                onCompleted: (pin) {
+                  // TODO: Firebase Verification
+                },
+                defaultPinTheme: PinTheme(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 16,
                   ),
-                  WhiteSpce(width: 5),
-                ],
+                  decoration: BoxDecoration(
+                    color: Colours.light,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  textStyle: GoogleFonts.poppins(
+                    fontSize: 18,
+                    color: Colours.darkBackground,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
             ],
           ),
